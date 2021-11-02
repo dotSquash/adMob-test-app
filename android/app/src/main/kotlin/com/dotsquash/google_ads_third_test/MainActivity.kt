50f6850ade52ffec7cb9a23098815ee0d02b83db
scottquashen <scottquashen.sq@gmail.com>
2021-11-01 19:14:14 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/dotsquash/google_ads_third_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dotsquash.google_ads_third_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:google_ads_third_test/admob_helper.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AdmobHelper.initialize();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admob ad example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatefulWidget {
+  @override
+  _MyHomepageState createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Admob Banner Test Ads'),
+      ),
+      bottomNavigationBar: Container(
+        child: AdWidget(
+          ad: AdmobHelper.getBannerAd()..load(),
+          key: UniqueKey(),
+        ),
+        height: 50,
+      ),
+    );
+  }
+}
